15cd5817678c0c0c8f735a9f7bafda776990797e
breadx333 <321.bd@mail.ru>
2023-10-19 18:40:42 +0300

    Addding DATABASEEEEE

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ class ActivityLogger:
 
 logger = ActivityLogger("activity_logger.txt")
 
-con = sqlite3.connect("pollen_database.db")
+con = sqlite3.connect("pollen_db.db", check_same_thread=False)
 cur = con.cursor()
 
+'''
 cur.execute("CREATE TABLE IF NOT EXISTS Bereza(date INTEGER, value INTEGER)")
 cur.execute("INSERT INTO Bereza (date, value) VALUES (1, 0)")
 res = cur.execute("SELECT * FROM Bereza")
 print(res.fetchone())
+'''
 
 """
 class pollen:
@@ -61,6 +63,21 @@ lon = "76.85"
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}'
 url_future = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={weather_token}'
 
+month_converter = {
+                    '01' : 'January', \
+                    '02' : 'February', \
+                    '03' : 'March', \
+                    '04' : 'April', \
+                    '05' : 'May', \
+                    '06' : 'June', \
+                    '07' : 'July', \
+                    '08' : 'August', \
+                    '09' : 'September', \
+                    '10' : 'October', \
+                    '11' : 'November', \
+                    '12' : 'December'
+}
+
 #Handler for /start command
 @bot.message_handler(commands=['start', 'back'])
 def start(message):
@@ -69,6 +86,13 @@ def start(message):
     keyboard.row('/now')
     keyboard.row('/additional')
     keyboard.row('/help')
+
+    username = message.from_user.username
+
+    #print(username)
+
+    #ABOBA NEXT LEVEL
+    #cur.execute(f"CREATE TABLE IF NOT EXISTS {username} (TEXT);")
 
     logger.log("start")
     
@@ -105,7 +129,18 @@ def now(message):
             + (2 if weather_id == 800 else koef[int(str(weather_id)[0])]) \
             + (hum * 0.02) \
             + (wind_speed * 1.1)) / 4
-        b = 1
+        
+        #result_from_db = cur.execute("SELECT * FROM April WHERE")
+        #cur.execute("SELECT * FROM April;")
+        rows = cur.execute(f"SELECT * FROM {month_converter[time.strftime("%m")]};").fetchall()
+        max_val = max(rows, key=lambda x: x[1])[1]
+
+        plants_with_max = ", ".join([x[0] for x in rows if x[1] == max_val])
+
+        #print(plants_with_max)
+        #print(month_converter[(datetime.datetime.today() + datetime.timedelta(days=43+31+31+29+31+30+31+30+31+31+30)).strftime("%m")])
+
+        b = max_val
 
         x = round(round(a, 2) * b) if b != 0 else 0
 
@@ -115,7 +150,8 @@ def now(message):
                                             \nDescription: {desc} \
                                             \nHumidity: {hum}% \
                                             \nWind Speed: {wind_speed} m/s \
-                                            \nDanger lever: {x}')
+                                            \nDanger lever: {x if x <= 2 else 2} \
+                                            \nFlowering plants: {plants_with_max if max_val != 0 else "no plants are blooming now"}')
     else:
         #Error if data is not fetched
         bot.reply_to(message, "Error fetching weather data")
@@ -159,11 +195,16 @@ def tomorrow(message):
 
         koef = {6 : 0.2, 2 : 0.4, 5 : 0.6, 3 : 0.8, 8 : 1, 7 : 1}
 
+        rows = cur.execute(f"SELECT * FROM {month_converter[time.strftime("%m")]};").fetchall()
+        max_val = max(rows, key=lambda x: x[1])[1]
+
+        plants_with_max = ", ".join([x[0] for x in rows if x[1] == max_val])
+
         a = ((temp * 0.05 if temp > 0 else 0.01) \
             + (2 if weather_id == 800 else koef[int(str(weather_id)[0])]) \
             + (hum * 0.02) \
             + (wind_speed * 1.1)) / 4
-        b = 1
+        b = max_val
 
         x = round(round(a, 2) * b) if b != 0 else 0
 
@@ -173,7 +214,8 @@ def tomorrow(message):
                                             \nDescription: {desc} \
                                             \nHumidity: {hum}% \
                                             \nWind Speed: {wind_speed} m/s \
-                                            \nDanger lever: {x}')
+                                            \nDanger lever: {x if x <= 2 else 2} \
+                                            \nFlowering plants: {plants_with_max if max_val != 0 else "no plants will be blooming"}')
 
 @bot.message_handler(commands=['threedays'])
 def threedays(message):
@@ -205,11 +247,16 @@ def threedays(message):
 
             koef = {6 : 0.2, 2 : 0.4, 5 : 0.6, 3 : 0.8, 8 : 1, 7 : 1}
 
+            rows = cur.execute(f"SELECT * FROM {month_converter[time.strftime("%m")]};").fetchall()
+            max_val = max(rows, key=lambda x: x[1])[1]
+
+            plants_with_max = ", ".join([x[0] for x in rows if x[1] == max_val])
+
             a = ((temp * 0.05 if temp > 0 else 0.01) \
                 + (2 if weather_id == 800 else koef[int(str(weather_id)[0])]) \
                 + (hum * 0.02) \
                 + (wind_speed * 1.1)) / 4
-            b = 1
+            b = max_val
 
             x = round(round(a, 2) * b) if b != 0 else 0
 
@@ -219,12 +266,15 @@ def threedays(message):
                                                 \nDescription: {desc} \
                                                 \nHumidity: {hum}% \
                                                 \nWind Speed: {wind_speed} m/s\
-                                                \nDanger lever: {x}')
+                                                \nDanger lever: {x if x <= 2 else 2} \
+                                                \nFlowering plants: {plants_with_max if max_val != 0 else "no plants will be blooming"}')
 
 @bot.message_handler(commands=['help'])
 def help(message):
     #Perform the button
-    bot.send_message(message.chat.id, "In order to use this bot please press start and request button")
+    bot.send_message(message.chat.id, "In order to use this bot please press start and request button \
+                                        \n \
+                                        \nHere are 3 danger levels to describe the widespread of allergens: 0, 1, 2")
 
     logger.log("help")
 
